705189627ba5f33bd5513880bf3e770f9c0331fe
kevint11 <kevintnatant@gmail.com>
2022-03-05 12:01:20 +0700

    12 01

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import {
   Routes,
   Route,
@@ -6,20 +7,22 @@ import {
 } from "react-router-dom";
 import Home from "./routes/home";
 import About from "./routes/about";
+import Footer from "./routes/footer";
 
 export default function App() {
   return (
-    <nav id="header" class="w-full z-30 top-0 text-black py-1 lg:py-6">
+    <React.Fragment>
+    <nav id="header" className="w-full z-30 top-0 text-black py-1 lg:py-6">
       <div
-        class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-2 lg:py-6"
+        className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-2 lg:py-6"
       >
-        <div class="pl-4 flex items-center">
+        <div className="pl-4 flex items-center">
           <a
-            class="text-black no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+            className="text-black no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
             href="#"
           >
             <svg
-              class="h-6 w-6 inline-block fill-current text-yellow-700"
+              className="h-6 w-6 inline-block fill-current text-yellow-700"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
             >
@@ -29,13 +32,13 @@ export default function App() {
           </a>
         </div>
 
-        <div class="block lg:hidden pr-4">
+        <div className="block lg:hidden pr-4">
           <button
             id="nav-toggle"
-            class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-green-500 appearance-none focus:outline-none"
+            className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-green-500 appearance-none focus:outline-none"
           >
             <svg
-              class="fill-current h-3 w-3"
+              className="fill-current h-3 w-3"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -46,30 +49,30 @@ export default function App() {
         </div>
 
         <div
-          class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 text-black p-4 lg:p-0 z-20"
+          className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 text-black p-4 lg:p-0 z-20"
           id="nav-content">
-          <ul class="list-reset lg:flex justify-end flex-1 items-center">
-            <li class="mr-3">
+          <ul className="list-reset lg:flex justify-end flex-1 items-center">
+            <li className="mr-3">
             <Link to="/"
-                class="inline-block py-2 px-4 text-black font-bold no-underline"
+                className="inline-block py-2 px-4 text-black font-bold no-underline"
                 href="#"
                 >Home</Link>
             </li>
-            <li class="mr-3">
+            <li className="mr-3">
             <Link to="/about"
-                class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
                 href="#"
                 >About App</Link>
             </li>
           </ul>
         </div>
       </div>
-    
-    <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
       </Routes>
-  
       </nav>
+      <Footer />
+      </React.Fragment>
   );
 }
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -15,73 +15,98 @@ export default function Home() {
   //   console.log(vals);
   // };
 
-  const [vals, setVals] = useState([])
+  const [vals, setVals] = useState([]);
   // const[message, setMessage] = useState('')
-  
+
   const {
     register,
     handleSubmit,
     reset,
-    formState: { isSubmitSuccessful }
+    formState: { isSubmitSuccessful },
   } = useForm({ defaultValues: { addTodo: "" } });
 
   const onSubmit = (data) => {
     let updatedValue = {};
     updatedValue = { title: data["addTodo"], status: 0 };
     setVals((vals) => [...vals, updatedValue]);
-    reset({ addTodo:'' })
+    reset({ addTodo: "" });
   };
 
-  console.log(vals);
+  const [done, setDone] = useState(0);
+  const toggleSS = (index) => {
+    console.log("index: " + index);
+    let newArr = [...vals];
+    newArr[index].status == 0
+      ? (newArr[index].status = 1)
+      : (newArr[index].status = 0);
+      
+    newArr[index].status == 0 ? 
+    (setDone(done-1))
+    : (setDone(done+1));
+    setVals(newArr);
+  };
+
+  const handleRemoveItem = (e) => {
+    const name = e.target.getAttribute("name");
+    setVals(vals.filter((val) => val.title !== name));
+  };
+
   return (
-  <main>
-    <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
-      <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
-        <div className="mb-4">
-          <h1 className="text-grey-darkest">Todo List</h1>
+    <main className="h-96">
+      <div className="h-100 w-auto flex items-center justify-center bg-teal-lightest font-sans">
+        <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+          <div className="mb-4">
+            <h1 className="text-grey-darkest">Todo List</h1>
 
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="flex mt-4">
-              <input {...register("addTodo", { required: true, maxLength: 20 })}
-                className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
-                placeholder="Add Todo"
-              />
-              <input
-                className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal"
-                type="submit"
-                value="Tambah"
-              />
-            </div>
-          </form>
-        </div>
-        <div>
-          {vals.map((val) => (
-            <div className="flex mb-4 items-center" key={val.title}>
-              <p className="w-full text-grey-darkest"> {val.title}</p>
-              <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">
-                Done
-              </button>
-              <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">
-                Remove
-              </button>
-            </div>
-          ))}
-
-          <div className="flex mb-4 items-center">
-            <p className="w-full line-through text-green">
-              Submit Todo App Component to Tailwind Components
-            </p>
-            <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey">
-              Not Done
-            </button>
-            <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">
-              Remove
-            </button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex mt-4">
+                <input
+                  {...register("addTodo", { required: true, maxLength: 20 })}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                  placeholder="Add Todo"
+                />
+                <input
+                  className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal"
+                  type="submit"
+                  value="Tambah"
+                />
+              </div>
+            </form>
           </div>
+          <div>
+            {vals.map((val, index) => (
+              <div className="flex mb-4 items-center" key={index.toString()}>
+                <p
+                  className={`w-full text-grey-darkest ${
+                    val.status == 1 ? "line-through" : ""
+                  }`}
+                >
+                  {" "}
+                  {val.title}
+                </p>
+                <button
+                  onClick={() => toggleSS(index)}
+                  className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green"
+                >
+                  {val.status == 1 ? "Not Done" : "Done"}
+                </button>
+                <button disabled={val.status == 1}
+                  name={val.title}
+                  onClick={handleRemoveItem}
+                  className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red"
+                >
+                  Remove
+                </button>
+                <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red"> Edit </button>
+              </div>
+            ))}
+
+           
+          </div>
+          <h2> Total to do : {vals.length} </h2>
+          <h2> Done : {done}</h2>
         </div>
       </div>
-    </div>
-  </main>
-  
+    </main>
   );
 }
